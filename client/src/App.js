57aa86d8fe625f193e3061221c2be986a2307e55
Reed Gaines <reed.gaines@gmail.com>
2018-04-01 19:22:08 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+var ReactS3Uploader = require('react-s3-uploader');
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imgURL: ""
+    }
+  }
+  
+  onUploadFinish = (img) => {
+    console.log(img)
+    this.setState({
+      imgURL: 'http://s3.amazonaws.com/sample-c4q/' + img.filename
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Upload an Image</h1>
+        </header>
+        <div className="image-holder">
+          <ReactS3Uploader
+            signingUrl="/s3/sign"
+            signingUrlMethod="GET"
+            accept="image/*"
+            uploadRequestHeaders={{
+              'x-amz-acl': 'public-read'
+            }}
+            onFinish={this.onUploadFinish}
+          />
+          
+          {this.state.imgURL ? <img src={this.state.imgURL}></img> : ""}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
