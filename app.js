ba61fc081058a8f308a34d616e47b7f73aaac945
Reed Gaines <reed.gaines@gmail.com>
2018-04-02 19:08:28 -0400

    add heroku stuff

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/s3', require('react-s3-uploader/s3router')({
     bucket: "sample-c4q",
